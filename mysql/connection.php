457d4bc2923007143ce90646c4bf76dc57828e48
wskksw <66136750+wskksw@users.noreply.github.com>
2023-03-25 12:42:35 -0700

    done some of the work

--- mysql/connection.php
+++ mysql/connection.php
@@ -5,9 +5,9 @@
 // $username = "root";
 // $password = "";
 $host = "cosc360.ok.ubc.ca";
-$dbname = "db_60578473";
-$username = "60578473";
-$password = "60578473";
+$dbname = "db_90108382";
+$username = "90108382";
+$password = "";
 $mysqli = new mysqli(hostname: $host,
                      username: $username,
                      password: $password,
