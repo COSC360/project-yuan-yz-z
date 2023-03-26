575482e6a5db350e6d8cdfd667b6f7d89c040dc4
wskksw <66136750+wskksw@users.noreply.github.com>
2023-03-25 21:17:01 -0700

    config

--- mysql/connection.php
+++ mysql/connection.php
@@ -1,13 +1,13 @@
 <?php
 
-// $host = "localhost";
-// $dbname = "cosc360";
-// $username = "root";
-// $password = "";
-$host = "cosc360.ok.ubc.ca";
-$dbname = "db_90108382";
-$username = "90108382";
+$host = "localhost";
+$dbname = "cosc360";
+$username = "root";
 $password = "";
+// $host = "cosc360.ok.ubc.ca";
+// $dbname = "db_90108382";
+// $username = "90108382";
+// $password = "";
 $mysqli = new mysqli(hostname: $host,
                      username: $username,
                      password: $password,
