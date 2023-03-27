8dae0866e173c89b01d4b6e28a6dbd4695a05bd7
yuan-yz-z <108699757+yuan-yz-z@users.noreply.github.com>
2023-03-27 15:44:45 -0700

    config

--- mysql/connection.php
+++ mysql/connection.php
@@ -1,13 +1,17 @@
 <?php
 
-$host = "localhost";
-$dbname = "cosc360";
-$username = "root";
-$password = "";
-// $host = "cosc360.ok.ubc.ca";
-// $dbname = "db_90108382";
-// $username = "90108382";
+error_reporting(E_ALL);
+
+in_set('display_errors', '1');
+
+// $host = "localhost";
+// $dbname = "cosc360";
+// $username = "root";
 // $password = "";
+$host = "localhost";
+$dbname = "db_60578473";
+$username = "60578473";
+$password = "60578473";
 $mysqli = new mysqli(hostname: $host,
                      username: $username,
                      password: $password,
